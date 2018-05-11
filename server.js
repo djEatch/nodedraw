a2a635962beb10753120cf73b3aab45b2765af0b
djEatch <duncan_eatch@hotmail.com>
2018-05-11 10:26:16 +0100

    hard coded port 3000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express();
-var port = process.env.PORT || 3000;
+var port = 3000;//process.env.PORT || 3000;
 console.log(port);
 var server = app.listen(port);
 
@@ -35,21 +35,21 @@ function newConnection(socket){
 // }).listen(port+1);
 
 
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-http.createServer(function (req, res) {
-    console.log(req);
-    var q = url.parse(req.url, true);
-    var filename = "." + q.pathname;
-    fs.readFile(filename, function(err, data) {
-      if (err) {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        return res.end("404 Not Found");
-      }  
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      return res.end();
-    });
-  }).listen(8080);
+// var http = require('http');
+// var url = require('url');
+// var fs = require('fs');
+// http.createServer(function (req, res) {
+//     console.log(req);
+//     var q = url.parse(req.url, true);
+//     var filename = "." + q.pathname;
+//     fs.readFile(filename, function(err, data) {
+//       if (err) {
+//         res.writeHead(404, {'Content-Type': 'text/html'});
+//         return res.end("404 Not Found");
+//       }  
+//       res.writeHead(200, {'Content-Type': 'text/html'});
+//       res.write(data);
+//       return res.end();
+//     });
+//   }).listen(8080);
 
